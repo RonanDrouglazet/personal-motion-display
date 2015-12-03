c10c7d03afb90ddd148e97960426f3396c168a83
Ronan Drouglazet <ronan.drouglazet@ebuzzing.com>
2015-12-03 23:37:18 +0100

    save

--- test2.py
+++ test2.py
@@ -6,6 +6,7 @@ import numpy as np
 import cv2
 import datetime
 import subprocess
+import string
 
 prior_image = None
 
@@ -73,8 +74,8 @@ with picamera.PiCamera() as camera:
                     camera.wait_recording(5)
                 print('Motion stopped!')
                 camera.split_recording(stream)
-                subprocess.call(['avconv', '-i', '"concat:before.h264|after.h264" -c copy ./motion/' + str(datetime.datetime.now()) + '.mp4'])
-                subprocess.call(['rm', '*.h264'])
+                subprocess.call(['avconv', '-i', '"concat:before.h264|after.h264" -c copy ./motion/' + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + '.mp4'])
+                subprocess.call(['rm', './*.h264'])
                 print('Encode Finish!')
     finally:
         camera.stop_recording()
