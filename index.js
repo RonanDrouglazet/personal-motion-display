fd6279517ba0bc212847d97d93d1a04f50f6a447
Ronan Drouglazet <ronan.drouglazet@ebuzzing.com>
2015-12-06 23:59:48 +0100

    save

--- index.js
+++ index.js
@@ -5,20 +5,13 @@ $(document).ready(function() {
     var fps = 12
     var running;
 
-    var image = function(story, name) {
-        var v = template.clone().attr('id', story);
+    var image = function(id, name) {
+        var v = template.clone().attr('id', id);
         v.find('.image').append('<img src="motion/' + name + '" />');
-        v.find('.header').html([
-            story.slice(0, 4),
-            story.slice(4, 6),
-            story.slice(6, 8)].join('/') + ' - ' + story.slice(10));
+        v.find('.header').html(name.slice(0, 10).replace('-', '/'));
 
-        var hour = name.match(/(?:\d+)-(?:\d{8})(\d{6})/)[1]
-        v.find('.date').html([hour.slice(0, 2), hour.slice(2, 4)].join(':') + '.' + hour.slice(4, 6) + 's')
-        v.find('.description').html((video[story].list.length * (1 / fps)).toFixed(1) + 's')
-        v.find('a.play').click(function() { load(story) })
+        v.find('.date').html(name.slice(11).replace('-', ':'))
         $('.ui.cards').append(v);
-        v.find('.progress').hide();
     }
 
     var cvideo = function(name) {
@@ -33,20 +26,11 @@ $(document).ready(function() {
         // filter story title
         filesArray.forEach(function(file) {
             if (file.match(/jpg$/)) {
-                /*var s = file.match(/(\d+)-(\d{10})/)
-                var num = s[1]
-                var day = s[2]
-                var story = day + num
-
-                if (!video[story]) {
-                    title.push([story, file])
-                    video[story] = {list: [{name: file}]}
-                } else {
-                    video[story].list.push({name: file})
-                }*/
-            } else if (file.match(/mp4$/)) {
+                var id = file.split('.')[0].replace('-', '')
+                title.push([parseInt(id), file])
+            } /*else if (file.match(/mp4$/)) {
                 cvideo(file)
-            }
+            }*/
         })
         // sort by date / story
         title = title.sort(function(a, b) {
@@ -56,66 +40,6 @@ $(document).ready(function() {
         title.forEach(function(infos) {
             image(infos[0], infos[1])
         })
-    }
-
-    var load = function(story) {
-        video[story].toLoad = 0;
-        video[story].loaded = 0;
-
-        video[story].list.forEach(function(image) {
-            if (!image.dom) {
-                video[story].toLoad++;
-                image.dom = document.createElement('img');
-                image.dom.src = 'motion/' + image.name;
-                image.dom.onload = progress.bind(this, story);
-            }
-        })
-
-        if (video[story].toLoad) {
-            $('#' + story).find('i.play').removeClass('play').addClass('spinner loading');
-        } else {
-            play(story);
-        }
-    }
-
-    var progress = function(story) {
-        video[story].loaded++
-
-        $('#' + story).find('.progress').show().progress({
-          percent: (video[story].loaded * 100) / video[story].toLoad
-        });
-
-        if (video[story].loaded === video[story].toLoad) {
-            //done
-            play(story);
-        }
-    }
-
-    var play = function(story) {
-        $('#' + story).find('i.spinner').removeClass('spinner loading').addClass('pause');
-
-        if (!video[story].canvas) {
-            var img = $('#' + story).find('img');
-            video[story].canvas = document.createElement('canvas');
-            video[story].canvas.width = img.width();
-            video[story].canvas.height = img.height();
-            video[story].context = video[story].canvas.getContext('2d');
-            img.replaceWith(video[story].canvas);
-        }
-
-
-        video[story].head = 0;
-
-        var playInterval = setInterval(function() {
-            video[story].context.drawImage(video[story].list[video[story].head].dom, 0, 0, video[story].canvas.width, video[story].canvas.height);
-            video[story].head++;
-
-            if (video[story].head === video[story].list.length) {
-                clearInterval(playInterval);
-                playInterval = null;
-                $('#' + story).find('i.pause').removeClass('pause').addClass('play');
-            }
-        }, 1000 / fps)
     }
 
     var status = function(started) {
