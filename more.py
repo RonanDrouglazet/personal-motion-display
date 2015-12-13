d43b3ad4746538608bd93aa068a85a04aaf0854a
Ronan Drouglazet <ronan.drouglazet@ebuzzing.com>
2015-12-13 18:32:08 +0100

    save

--- more.py
+++ more.py
@@ -172,7 +172,7 @@ class SDEncode(Thread):
                 print videos
             else:
                 print ('wait encode SD')
-                videos = subprocess.call(['ls', '-l', 'motion/*.mp4'])
+                videos = subprocess.call(['ls', '-l', motion_path])
                 print videos
                 time.sleep(story_duration * 0.1)
         
