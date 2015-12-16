c0a5d319b04934b7358abeac042ab300d988c01f
RonanDrouglazet <ronan.drouglazet@ebuzzing.com>
2015-12-16 22:20:09 +0100

    save

--- day.py
+++ day.py
@@ -1,10 +1,11 @@
 import picamera
+import time
 
 with picamera.PiCamera() as camera:
     camera.resolution = (1280, 720)
     camera.vflip = True
     camera.hflip = True
-    # Finally, capture an image with a 6s exposure. Due
-    # to mode switching on the still port, this will take
-    # longer than 6 seconds
+    camera.start_preview()
+    # Camera warm-up time
+    time.sleep(2)
     camera.capture('/home/pi/personal-motion-display/motion/day.jpg')
