ab6024dd27cc8e6c06c39e7c8d0066c4e97782ba
RonanDrouglazet <ronan.drouglazet@ebuzzing.com>
2015-12-16 21:24:04 +0100

    save

--- index.js
+++ index.js
@@ -1,29 +1,18 @@
 $(document).ready(function() {
-    var template = $('.ui.card').remove()
+    var card = $('.ui.card')
     var title = []
-    var video = {}
-    var fps = 12
     var running;
 
+    // Dimmer
     var image = function(id, name) {
-        var v = template.clone().attr('id', name);
-        v.find('.image').append('<video width="290" src="motion/' + name.replace('.jpg', '-HD.mp4') + '" poster="motion/' + name + '" controls preload="none"/>');
-        v.find('.header').html(name.slice(0, 10).replace(/-/ig, '/'));
+        card.find('.image').html('<video width="290" src="motion/' + name.replace('.jpg', '-HD.mp4') + '" poster="motion/' + name + '" controls preload="none"/>');
+        card.find('.header').html(name.slice(0, 10).replace(/-/ig, '/'));
 
-        v.find('.date').html(name.split('.').shift().slice(11).replace('-', ':').replace('-', '.'))
-        $('.ui.cards').append(v);
+        card.find('.date').html(name.split('.').shift().slice(11).replace('-', ':').replace('-', '.'))
 
-        v.find('.checkbox')
-          .checkbox({
-            onChecked: function() {
-              v.find('video').attr('src', 'motion/' + name.replace('.jpg', '-HD.mp4'))
-            },
-            onUnchecked: function() {
-              v.find('video').attr('src', 'motion/' + name.replace('.jpg', '-SD.mp4'))
-            }
-          })
-        
-        v.find('.trash').click(function() {
+        card.find('.trash').off()
+        card.find('.trash').click(function() {
+            $('.dimmer').dimmer('hide')
             $('.ui.basic.modal').modal({
                 onDeny    : function(){
                   return true;
@@ -31,33 +20,47 @@ $(document).ready(function() {
                 onApprove : function() {
                   $.get('/api/remove/' + name, function(err) {
                     if (!err) {
-                        v.remove();
+                        $('.' + id).remove();
                     }
                   })
                 }
               }).modal('show')
         })
 
-        v.find('.save').click(function() {
+        card.find('.save').off()
+        card.find('.save').click(function() {
+            $('.dimmer').dimmer('hide')
             $('.ui.basic.modal').modal({
                 onDeny    : function(){
                   return true;
                 },
                 onApprove : function() {
-                  $.get('/api/keep/' + name.replace('.jpg', ''), function(err) {
-                    if (!err) {
-                        v.find('.save').remove();
-                    }
-                  })
+                  $.get('/api/keep/' + name.replace('.jpg', ''), function(err) {})
                 }
               }).modal('show')
         })
     }
 
+    // button per motion
+    var button_motion = function(id, name) {
+        hour = name.split('.')[0].split('-').slice(3, 5).join(':')
+        $('.ui.container').append('<button class="ui inverted button ' + id + '">' + hour + '</button>')
+        $('button').last().click(function() {
+            image(id, name)
+            $('.dimmer').dimmer('show')
+        })
+    }
+
+    // Divider per day
+    var divider = function(day) {
+        $('.ui.container').append('<div class="ui inverted horizontal divider">' + day + '</div>')
+    }
+
+    // creation process
     var create = function(filesArray) {
         // filter story title
         filesArray.forEach(function(file) {
-            if (file.match(/jpg$/)) {
+            if (file.match(/(\d+-){5}\d+\.jpg$/)) {
                 var id = file.split('.')[0].replace(/-/ig, '')
                 title.push([parseInt(id), file])
             }
@@ -67,31 +70,41 @@ $(document).ready(function() {
         title = title.sort(function(a, b) {
             return parseInt(a[0]) < parseInt(b[0]) ? 1 : -1
         })
+
+        var day;
         // append
         title.forEach(function(infos) {
-            image(infos[0], infos[1])
+            if (!day || infos[1].indexOf(day) === -1) {
+                day = infos[1].split('.')[0].split('-').slice(0, 3).join('-')
+                divider(day)
+            }
+            button_motion(infos[0], infos[1])
         })
     }
 
+    // set motion record button status
     var status = function(started) {
         $(started ? '.green' : '.red').removeClass(started ? 'green' : 'red').addClass(started ? 'red' : 'green');
-        $('.labeled.button').find('.button').html(started ? 
+        $('.labeled.button').find('.button').html(started ?
             '<i class="stop icon"></i> Stop' :
             '<i class="play icon"></i> Start');
         $('.labeled.button').find('.label').html(started ? 'started' : 'stopped');
     }
 
+    // retrieve list of motion
     $.get('/api/list', function(data) {
         if (!data.error && data.files) {
             create(data.files)
         }
     })
 
+    // get status of motion record
     $.get('/api/status', function(started) {
         running = started
         status(started)
     })
 
+    // set motion record status (start / stop)
     $('.labeled.button').find('.button').click(function() {
         $(this).find('i').removeClass('play stop').addClass('spinner loading')
         $.get('/api/' + (running ? 'stop' : 'start'), function(err) {
